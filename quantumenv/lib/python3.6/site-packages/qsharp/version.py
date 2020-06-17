5e704ead98629963f6c813427b06638ac265d2b6
sahil9001 <sahilsilare@gmail.com>
2020-06-17 12:18:14 +0530

    new file:   .gitignore 	new file:   .vscode/settings.json 	new file:   Bell.qs 	new file:   host.py

---
+++ quantumenv/lib/python3.6/site-packages/qsharp/version.py
@@ -0,0 +1,9 @@
+# Auto-generated file, do not edit.
+##
+# version.py: Specifies the version of the qsharp package.
+##
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+##
+__version__ = "0.11.2006.403"
+_user_agent_extra = ""
